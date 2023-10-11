975c31492bb5252927c555913cfd6f64b201d828
myildiri <mete.yildirim@boc-group.com>
2023-10-11 09:04:18 +0200

    add more folder

---
+++ car-rent/src/templates/user-template.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/user/common/header/header";
+import Footer from "../components/user/common/footer/footer";
+
+const UserTemplate = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default UserTemplate;
