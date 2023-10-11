81e88c27b1d531b306f910c7017f19704594575e
myildiri <mete.yildirim@boc-group.com>
2023-10-11 08:02:46 +0200

    add settings.js

---
+++ car-rent/src/helpers/settings.js
@@ -0,0 +1,11 @@
+export const settings = {
+    apiURL :"https://carrental-v3-backend.heroku.com",
+    siteName : "Let's Rental Car",
+    siteUrl: "letsrentalcar.com",
+    email: "support@letsrentalcar.com",
+    phone1: "(845)-356-0642",
+    phone2: "(845)-556-7624",
+    address:"431 5th Ave, New York, NY 10016, USA",
+    mapEmbedUrl: "https://www.google.com/maps/embed?pb=!"
+
+}
