c8887518c46bb85ed61aa67d537bd6c698b57dc3
myildiri <mete.yildirim@boc-group.com>
2023-10-17 08:14:31 +0200

    Common pages added

---
+++ car-rent/src/pages/common/unauthorized-page.js
@@ -0,0 +1,8 @@
+import React from "react";
+import UserTemplate from "../../templates/user-template";
+
+const UnauthorizedPage = () => {
+  return <UserTemplate>UnauthorizedPage</UserTemplate>;
+};
+
+export default UnauthorizedPage;
