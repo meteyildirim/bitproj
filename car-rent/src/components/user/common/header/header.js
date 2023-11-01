899ac389b375e0e65eedb9a15f3c92f6ef8663bc
myildiri <mete.yildirim@boc-group.com>
2023-11-01 13:56:08 +0100

    add logo

--- car-rent/src/components/user/common/header/header.js
+++ car-rent/src/components/user/common/header/header.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Container } from "react-bootstrap";
+// import styled from "styled-components";
+import logo from "../../../../assets/img/logo/logo.png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Container>
+        <div className="header">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default Header;
