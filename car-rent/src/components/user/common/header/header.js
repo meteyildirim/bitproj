bb0688e50baea0e1127eb1e25cffabd3fcc283b1
myildiri <mete.yildirim@boc-group.com>
2023-11-01 15:35:44 +0100

    header logo eklendi, menubar ve topbar componentler olusturuldu

--- car-rent/src/components/user/common/header/header.js
+++ car-rent/src/components/user/common/header/header.js
@@ -3,14 +3,20 @@ import { Container } from "react-bootstrap";
 // import styled from "styled-components";
 import logo from "../../../../assets/img/logo/logo.png";
 import { settings } from "../../../../helpers/settings";
+import Menubar from "./menubar";
+import Topbar from "./topbar";
 const Header = () => {
   return (
     <>
       <Container>
         <div className="header">
           <div className="logo">
-            <img src={logo} alt={settings.siteName} className="img-fluid"/>
+            <img src={logo} alt={settings.siteName} className="img-fluid" />
           </div>
+        </div>
+        <div className="menus">
+          <Menubar />
+          <Topbar />
         </div>
       </Container>
     </>
