aa2ac5839cf57937cf01853f3385d7502b0bd114
myildiri <mete.yildirim@boc-group.com>
2023-11-01 11:53:20 +0100

    provide store to main application

--- car-rent/src/App.js
+++ car-rent/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import CustomRoutes from "./router/custom-routes";
+import { Provider as StoreProvider } from "react-redux";
+import store from "./store"
 
 const App = () => {
   return (
     <>
-      <CustomRoutes />
+      <StoreProvider store={store}>
+        <CustomRoutes />
+      </StoreProvider>
     </>
   );
 };
