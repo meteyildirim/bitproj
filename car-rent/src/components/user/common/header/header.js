3324aeabfa96986d20f6d4a61d04585443082820
myildiri <mete.yildirim@boc-group.com>
2023-11-01 14:00:05 +0100

    logo alt text added

--- car-rent/src/components/user/common/header/header.js
+++ car-rent/src/components/user/common/header/header.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Container } from "react-bootstrap";
 // import styled from "styled-components";
 import logo from "../../../../assets/img/logo/logo.png";
+import { settings } from "../../../../helpers/settings";
 const Header = () => {
   return (
     <>
       <Container>
         <div className="header">
           <div className="logo">
-            <img src={logo} alt="" />
+            <img src={logo} alt={settings.siteName} className="img-fluid"/>
           </div>
         </div>
       </Container>
