59225bacf511f8c9d599762255017b80d001aaf5
myildiri <mete.yildirim@boc-group.com>
2023-11-01 11:45:04 +0100

    remove duplicated line

--- car-rent/src/store/slices/auth-slice.js
+++ car-rent/src/store/slices/auth-slice.js
@@ -14,7 +14,6 @@ export const authSlice = createSlice({
   reducers: {
     loginSuccess: (state, action) => {
       state.isUserLogin = true;
-      state.isUserLogin = true;
       state.user = action.payload;
     },
     loginFailled: (state) => {
