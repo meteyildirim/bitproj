bb8fba5bb0eb0bd33a51aa42703dea3effb111bf
myildiri <mete.yildirim@boc-group.com>
2023-11-03 07:56:45 +0100

    Loading page and not found page styling

---
+++ car-rent/src/components/common/loading/loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Spinner } from "react-bootstrap";
+
+import "./loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <Spinner animation="border" />
+    </div>
+  );
+};
+
+export default Loading;
--- car-rent/src/pages/common/not-found-page.js
+++ car-rent/src/pages/common/not-found-page.js
@@ -1,15 +1,12 @@
 import React from "react";
 import UserTemplate from "../../templates/user-template";
-import { Col, Container, Row } from "react-bootstrap";
-import "./notfound.scss";
+import Notfound from "../../components/common/notfound/notfound";
+
 const NotFoundPage = () => {
   return (
     <>
       <UserTemplate>
-        <div className="nofound404">
-          <h1 className="text404">404</h1>
-          <h3 className="message">The page you are looking for is not found</h3>
-        </div>
+        <Notfound />
       </UserTemplate>
       ;
     </>
