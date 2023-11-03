767e5af078a9034408ad89879aaa9c241efc8354
myildiri <mete.yildirim@boc-group.com>
2023-11-03 06:49:26 +0100

    Loading page and not found page

--- car-rent/src/pages/common/loading-page.js
+++ car-rent/src/pages/common/loading-page.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Spinner } from "react-bootstrap";
+import logo from "../../assets/img/logo/logo.png";
 
 const LoadingPage = () => {
   return (
-    <div>LoadingPage</div>
-  )
-}
+    <div className="loading-page">
+      <Spinner animation="border" variant="primary" />
+      <img src={logo} alt={"Loading..."} />
+    </div>
+  );
+};
 
-export default LoadingPage
+export default LoadingPage;
--- car-rent/src/pages/common/not-found-page.js
+++ car-rent/src/pages/common/not-found-page.js
@@ -1,8 +1,19 @@
 import React from "react";
 import UserTemplate from "../../templates/user-template";
-
+import { Col, Container, Row } from "react-bootstrap";
+import "./notfound.scss";
 const NotFoundPage = () => {
-  return <UserTemplate>NotFoundPage</UserTemplate>;
+  return (
+    <>
+      <UserTemplate>
+        <div className="nofound404">
+          <h1 className="text404">404</h1>
+          <h3 className="message">The page you are looking for is not found</h3>
+        </div>
+      </UserTemplate>
+      ;
+    </>
+  );
 };
 
 export default NotFoundPage;
